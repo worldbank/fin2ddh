7acf35b7f4f3201cd9ba95c8f3d7c4f42ee8329f
meera <mbdesai@umich.edu>
2017-10-13 15:28:19 -0400

    replaced flat files with ddhconnect calls

--- data-raw/add_internal_data.R
+++ data-raw/add_internal_data.R
@@ -3,19 +3,34 @@ library(jsonlite)
 library(magrittr)
 # using code from mdlibtoddh
 
+# CHECK consistency between STG & PROD taxonomy
+# check_taxonomy_services()
+# Set root_url URL
+# currently pulling from staging
+# root_url <- ddhconnect:::production_root_url
+root_url <- "https://newdatacatalogstg.worldbank.org/"
+
 # STEP 1: Get data --------------------------------------------------------
 
 # Metadata from flat files
 httr::set_config(httr::config(ssl_verifypeer = 0L))
 
 finance_lovs <- readr::read_csv("finance_lovs.csv")
-ddh_lovs <- readxl::read_excel("ddh_lovs.xlsx")
-# building off of this one
+
+# ddh_lovs <- readxl::read_excel("ddh_lovs.xlsx")
+ddh_lovs <- ddhconnect::get_lovs(root_url = root_url)
+#   %>%rename(ddh_machine_name = machine_name, field_lovs = list_value_name)
+names(ddh_lovs)[names(ddh_lovs) == "ddh_machine_name"] <- "machine_name"
+names(ddh_lovs)[names(ddh_lovs) == "field_lovs"] <- "list_value_name"
+  
 ddh_finance_map <- readxl::read_excel("ddh_finance_map.xlsx")
+#names(ddh_finance_map)[names(ddh_finance_map) == "ddh_json_key"] <- "machine_name"
 
-names(ddh_finance_map)[names(ddh_finance_map) == 'ddh_json_key'] <- 'machine_name'
+ddh_fields <- ddhconnect::get_fields(root_url = root_url) 
+# %>% rename(ddh_machine_name = machine_name)
+names(ddh_fields)[names(ddh_fields) == "ddh_machine_name"] <- "machine_name"
 
-lookup <- ddh_finance_map
+lookup <- ddh_fields
 
 # STEP 2: Check data consistency ------------------------------------------
 # lookup & finance
@@ -26,11 +41,16 @@ lookup <- ddh_finance_map
 
 
 # STEP 3: Merge data ------------------------------------------------------
+# keep fields look up?
+#fin_lookup <- unique(subset(ddh_fields, select = c("machine_name"))) %>%
+#          full_join(ddh_lovs, by = "machine_name") %>%
+#          left_join(ddh_finance_map, by = "machine_name") %>%
+#          left_join(finance_lovs, by = c("machine_name", "list_value_name"))
 
-# Merge lookup and mdlib
-lookup <- lookup %>%
-          full_join(ddh_lovs, by = "machine_name") %>%
-          left_join(finance_lovs, by = c("machine_name", "list_value_name"))
+fin_lookup <- ddh_finance_map %>%
+              full_join(ddh_lovs, by = "machine_name") %>%
+              left_join(finance_lovs, by = c("machine_name", "list_value_name"))
+
 
 
 # Save table -------------------------------------------------------

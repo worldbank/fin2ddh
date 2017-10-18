b9f05561299df2ba6cced8e3340a7b0cd4b7316f
Karthik Ramanathan Dhanalakshmi Ramanathan <kdhanalakshmiram@worldbank.org>
2017-10-18 09:53:55 -0400

    rename add_new_dataset()

--- R/fin_to_ddh_keys.R
+++ R/fin_to_ddh_keys.R
@@ -15,14 +15,14 @@ fin_to_ddh_keys <- function(metadata_in,
                                 metadata_out = mdlibtoddh::md_placeholder,
                                 lookup = fin2ddh::lookup) {
   machine_names <- sort(names(metadata_out))
-  fin_keys_lookup <- lookup %>% dplyr::select(machine_name, finance_json_key, list_value_name, finance_value, tid) %>% dplyr::filter(!is.na(finance_json_key))
-  lookup_unique <- dplyr::distinct(fin_keys_lookup, machine_name, finance_json_key)
-  fin_machine_names <- lookup_unique$machine_name
-  h <- hash::hash(keys = lookup_unique$machine_name, values = lookup_unique$finance_json_key)
+  fin_keys_lookup <- lookup %>% dplyr::filter(!is.na(finance_json_key))
+  lookup_unique <- dplyr::distinct(fin_keys_lookup, ddh_machine_name, finance_json_key)
+  fin_machine_names <- lookup_unique$ddh_machine_name
+  h <- hash::hash(keys = lookup_unique$ddh_machine_name, values = lookup_unique$finance_json_key)
 
   for(machine_name in fin_machine_names) {
-    fin_name <- h[[machine_name]]
-    metadata_out[[machine_name]] <- metadata_in[[fin_name]]
+    fin_name_raw <- h[[machine_name]]
+    metadata_out[[machine_name]] <- format_lookup(metadata_in, fin_name_raw)
   }
 
   for(machine_name in ddhconnect:::mandatory_text_fields) {
@@ -32,4 +32,10 @@ fin_to_ddh_keys <- function(metadata_in,
   }
 
   return(metadata_out)
+}
+
+format_lookup <- function(metadata_in, lookup_name) {
+  keys <- unlist(strsplit(lookup_name, "/"))
+  values <- purrr::at_depth(metadata_in, .depth = 0, .f = keys)
+  return(values$view)
 }

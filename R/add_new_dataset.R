d79c76dcdc912150fc4aea2de19724f066660c41
Karthik Ramanathan Dhanalakshmi Ramanathan <kdhanalakshmiram@worldbank.org>
2017-10-23 12:04:18 -0400

    temp commit

--- R/add_new_dataset.R
+++ R/add_new_dataset.R
@@ -10,7 +10,7 @@
 #' @export
 #'
 
-add_new_dataset <- function(root_url) {
+add_new_dataset <- function(root_url, ddh_credentials = list(cookie = dkanr::get_cookie(), token = dkanr::get_token())) {
   # STEP 1: Collect data from API
   temp <- extract_fin_metadata(root_url)
 
@@ -18,9 +18,31 @@ add_new_dataset <- function(root_url) {
   temp <- filter_fin_metadata(temp)
 
   # STEP 3: MAP to keys and flatten
-  mapped_finance <- temp[["results"]] %>%
-    purrr::map(fin_to_ddh_keys) %>%
-    purrr::map(add_constant_metadata) %>%
-    purrr::map(map_fin_metadata)
-  return(mapped_finance)
+  # mapped_finance <- temp[["results"]] %>%
+  #   purrr::map(fin_to_ddh_keys) %>%
+  #   purrr::map(add_constant_metadata) %>%
+  #   purrr::map(map_fin_metadata)
+
+  for(i in 1:length(temp[["results"]])) {
+    metadata_temp <- fin_to_ddh_keys(temp[["results"]][[i]])
+    metadata_temp <- add_constant_metadata(metadata_temp)
+    metadata_temp <- map_fin_metadata(metadata_temp)
+
+    category <- temp[["results"]][[i]]$view$category
+    metadata_temp <- add_link_to_resources(metadata_temp, category)
+
+    json_dat <- create_json_dataset(metadata_temp)
+    resp_dat <- ddhconnect::create_dataset(credentials = ddh_credentials, body = json_dat, root_url = root_url)
+
+    json_res <- create_json_resource(metadata_temp)
+    resp_res <- ddhconect::create_resource(credentials = ddh_credentials, body = json_res, root_url = root_url)
+
+    json_attach <- create_json_attach(metadata_list = metadata_temp, resource_nid = resp_res$node_id)
+    resp_attach <- attach_resource_to_dataset(credentials = ddh_credentials,
+                                              dataset_nid = resp_dat$node_id,
+                                              body = json_attach,
+                                              root_url = root_url)
+  }
+
+  return(resp_dat$uri)
 }
---
+++ tests/testthat/test-create_json_dataset.R
@@ -0,0 +1,13 @@
+context("test-create_json_dataset.R")
+
+test_that("dataset has all columns in the correct format", {
+  ddh_credentials <- list(cookie = dkanr::get_cookie(), token = dkanr::get_token())
+  ddh_root_url <- dkanr::get_url()
+  metadata_list <- jsonlite::fromJSON("./data-raw/metadata_test.json")
+
+  json_dat <- create_json_dataset(metadata_list)
+  resp_dat <- ddhconnect::create_dataset(credentials = ddh_credentials, body = json_dat, root_url = root_url)
+  node_metadata <- ddhconnect::get_metadata(resp_dat$node_id, ddh_credentials, root_url)
+
+  expect_equal(node_metadata$title, metadata_list$title)
+})

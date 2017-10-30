8a23d11632301d7963485d4c090fc5b5d5130a52
meera <mbdesai@umich.edu>
2017-10-30 13:40:44 -0400

    update tag formatting

--- R/add_constant_metadata.R
+++ R/add_constant_metadata.R
@@ -23,5 +23,6 @@ add_constant_metadata <- function(metadata_list) {
   metadata_list$field_ddh_external_contact_email <- 'wbfinances@worldbank.org'
   metadata_list$field_wbddh_source <- 'World Bank Group'
   metadata_list$field_wbddh_data_type <- 'Other'
+  metadata_list$field_tags <- 'finances.worldbank.org'
   return(metadata_list)
 }

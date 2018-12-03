0c663a5c6f002538989040e1ac243971f62356d5
seladore <mbdesai@umich.edu>
2018-12-03 17:46:03 -0500

    add call to add new dataset

--- R/add_new_dataset.R
+++ R/add_new_dataset.R
@@ -48,6 +48,11 @@ add_new_dataset <- function(metadata_list,
                                                            resource_nids = c(resp_res$nid),
                                                            root_url = root_url,
                                                            credentials = credentials)
+
+    # test created dataset
+    metadata_dataset <- ddhconnect::get_metadata(nid = resp_dat$nid,
+                                                 root_url = root_url,
+                                                 credentials = credentials)
     test_created_dataset(dataset_metadata = metadata_dataset,
                          metadata_list = metadata_temp,
                          root_url = root_url,

962e8ff7ed9fda984800f881259f2546eba1cf8e
Alp Arslan Aziz <aaziz3@worldbank.org>
2019-04-25 15:10:47 -0400

    small fix in trycatch()

--- R/add_new_dataset.R
+++ R/add_new_dataset.R
@@ -63,11 +63,12 @@ add_new_dataset <- function(metadata_list,
                            root_url = root_url,
                            credentials = credentials)
 
-      return(resp_dat$uri)
+      return(cat(resp_dat$uri))
 
     }, error = function(e){
 
-      return(paste("Error:",e,"; with creating resources for", resp_dat$uri))
+      message <- paste("Error:",e,"; with creating resources for", resp_dat$uri)
 
+      return(cat(message))
     })
   }
